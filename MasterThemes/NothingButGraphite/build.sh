48e4b832d480ad60b15a068dcd969c46116b67b5
hibenji <65447501+hibenji@users.noreply.github.com>
2020-08-27 13:15:12 +0200

    Update build.sh

--- MasterThemes/NothingButGraphite/build.sh
+++ MasterThemes/NothingButGraphite/build.sh
@@ -21,7 +21,7 @@ if [ ! -f "artisan" ]; then
     zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources
 
     echo "Downloading the Theme you picked"
-    mkdir -p tempdown && cd tempdown && git clone https://github.com/TheFonix/Pterodactyl-Themes.git .
+    mkdir -p tempdown && cd tempdown && git clone https://github.com/hibenji/Pterodactyl-Themes.git .
     cp -r MasterThemes/NothingButGraphite/public ..
     
 
